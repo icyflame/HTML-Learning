bc3f081965071f90a00d3d87e4bf4e9ddc6a0d00
IcyFlame <kannan.siddharth12@gmail.com>
2013-07-01 14:21:39 +0530

    changed gitPush.sh, will push only to the master branch. Will not ask for the branch before pushing.

--- gitPush.sh
+++ gitPush.sh
@@ -3,14 +3,12 @@
 #will try to push all the changes made till now to the server.
 
 echo
-echo 'enter the name of the branch:'
-read branch
 
 echo 'trying to push data to server:'
 echo '=================================='
 echo '=================================='
 
-git push origin $branch
+git push origin master
 
 echo '=================================='
 echo '=================================='
